9ab142e259318577edc7cf5a8851a411b06ea1ea
nokya <n0ky4@proton.me>
2025-01-19 20:04:51 -0300

    [feat] add link button, add light theme

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const themeSwitcherBtn = document.querySelector('#theme-switcher')
 
 themeSwitcherBtn.addEventListener('click', () => {
+    const html = document.querySelector('html')
+    html.classList.toggle('light')
+
     themeSwitcherBtn.classList.toggle('active')
 })
