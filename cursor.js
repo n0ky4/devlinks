79fe12b7b85e16601f27938005d5a75a3b67379b
nokya <n0ky4@proton.me>
2025-01-19 21:15:45 -0300

    [ref] add separate cursor javascript file

---
+++ cursor.js
@@ -0,0 +1,39 @@
+// Usar regex para detectar se o usuário está no Mobile
+const isMobile = () => {
+    const userAgent = navigator.userAgent
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)
+}
+
+let cursorEnabled = !isMobile()
+
+const cursor = document.querySelector('.cursor')
+
+window.addEventListener('resize', () => {
+    cursorEnabled = !isMobile()
+})
+
+const interactiveElements = ['A', 'BUTTON', 'INPUT', 'TEXTAREA']
+const isHoverInteractiveElement = (x, y) => {
+    const element = document.elementFromPoint(x, y)
+    return interactiveElements.includes(element.tagName)
+}
+
+window.addEventListener('mousemove', (e) => {
+    if (!cursorEnabled) {
+        cursor.classList.add('hidden')
+        return
+    }
+
+    if (cursor.classList.contains('hidden')) cursor.classList.remove('hidden')
+
+    const x = e.clientX
+    const y = e.clientY
+    cursor.style.left = `${x}px`
+    cursor.style.top = `${y}px`
+
+    if (isHoverInteractiveElement(x, y)) {
+        cursor.classList.add('hover')
+    } else {
+        cursor.classList.remove('hover')
+    }
+})
--- index.js
+++ index.js
@@ -1,45 +1,7 @@
-// Usar regex para detectar se o usuário está no Mobile
-const isMobile = () => {
-    const userAgent = navigator.userAgent
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)
-}
-
-let cursorEnabled = !isMobile()
 const themeBtn = document.querySelector('#theme-switcher')
-const cursor = document.querySelector('.cursor')
 
 themeBtn.addEventListener('click', () => {
     const html = document.querySelector('html')
     html.classList.toggle('light')
     themeBtn.classList.toggle('active')
-})
-
-window.addEventListener('resize', () => {
-    cursorEnabled = !isMobile()
-})
-
-const interactiveElements = ['A', 'BUTTON', 'INPUT', 'TEXTAREA']
-const isHoverInteractiveElement = (x, y) => {
-    const element = document.elementFromPoint(x, y)
-    return interactiveElements.includes(element.tagName)
-}
-
-window.addEventListener('mousemove', (e) => {
-    if (!cursorEnabled) {
-        cursor.classList.add('hidden')
-        return
-    }
-
-    if (cursor.classList.contains('hidden')) cursor.classList.remove('hidden')
-
-    const x = e.clientX
-    const y = e.clientY
-    cursor.style.left = `${x}px`
-    cursor.style.top = `${y}px`
-
-    if (isHoverInteractiveElement(x, y)) {
-        cursor.classList.add('hover')
-    } else {
-        cursor.classList.remove('hover')
-    }
 })
