b70376e6082b1e693f31b70b19b045fe15d336a7
nokya <n0ky4@proton.me>
2025-01-19 22:23:21 -0300

    [feat] add three.js background

---
+++ bg.js
@@ -0,0 +1,79 @@
+import * as THREE from 'three'
+
+const CONFIG = {
+    enabled: true,
+    antialias: false,
+    colors: {
+        dark: 'white',
+        light: 'black',
+    },
+    rotationSpeed: 0.0015,
+}
+
+function setup() {
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    )
+
+    const renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: CONFIG.antialias,
+    })
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    document.body.appendChild(renderer.domElement)
+
+    // Atualizar o tamanho do canvas quando redimensionar
+    window.addEventListener('resize', () => {
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+    })
+
+    return {
+        scene,
+        camera,
+        renderer,
+    }
+}
+
+function main() {
+    const { scene, camera, renderer } = setup()
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+    const material = new THREE.MeshBasicMaterial({ color: CONFIG.colors.dark, wireframe: true })
+
+    const cube1 = new THREE.Mesh(geometry, material)
+    const cube2 = new THREE.Mesh(geometry, material)
+
+    // PI = 180 graus (dividido por 1/4 (0.25) = 45 graus)
+    cube2.rotation.y = Math.PI * 0.25 // three.js usa radianos
+    scene.add(cube1, cube2)
+
+    const rotate = (obj, dec) => {
+        const multip = dec === true ? -1 : 1
+        obj.rotation.x += CONFIG.rotationSpeed * multip
+        obj.rotation.y += CONFIG.rotationSpeed * multip
+    }
+
+    onThemeChange((newTheme) => {
+        const color = newTheme === 'dark' ? CONFIG.colors.dark : CONFIG.colors.light
+        material.color.set(color)
+    })
+
+    function animate() {
+        rotate(cube1)
+        rotate(cube2, true)
+        renderer.render(scene, camera)
+    }
+
+    renderer.setAnimationLoop(animate)
+}
+
+if (CONFIG.enabled) main()
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+const themeChangeCallbacks = []
+
 const themeBtn = document.querySelector('#theme-switcher')
+
+const onThemeChange = (callback) => {
+    if (!themeChangeCallbacks.includes(callback)) themeChangeCallbacks.push(callback)
+}
+
+function callThemeCallbacks(newTheme) {
+    themeChangeCallbacks.forEach((cb) => {
+        cb(newTheme)
+    })
+}
 
 themeBtn.addEventListener('click', () => {
     const html = document.querySelector('html')
+
+    const newTheme = html.classList.contains('light') ? 'dark' : 'light'
     html.classList.toggle('light')
     themeBtn.classList.toggle('active')
+
+    callThemeCallbacks(newTheme)
 })
